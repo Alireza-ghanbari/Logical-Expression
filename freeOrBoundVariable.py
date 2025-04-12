ee69a3de729b150ef7faaad847e8d634a2d38562
Alireza Ghanbari <alirezaghanbari.dev@gmail.com>
2025-04-12 22:23:46 +0430

    Update freeOrBoundVariable.py

--- freeOrBoundVariable.py
+++ freeOrBoundVariable.py
@@ -6,59 +6,55 @@ def extract_variables(expression):
     n = len(expression)
 
     while i < n:
-        if expression[i] in ('∀', '∃'):
+        char = expression[i]
+
+        if char in ('∀', '∃'):
             i += 1
-            var = expression[i]
-            if scopes:
-                scopes[-1].add(var)
-            else:
-                scopes.append(set([var]))
-            i += 1
-            if i < n and expression[i] == '(':
-                scopes.append(set([var]))
+            if i < n:
+                var = expression[i]
+                if scopes:
+                    scopes[-1].add(var)
+                else:
+                    scopes.append(set([var]))
                 i += 1
-        elif expression[i] == '(':
+                if i < n and expression[i] == '(':
+                    scopes.append(set([var]))
+                    i += 1
+
+        elif char == '(':
             scopes.append(set())
             i += 1
-        elif expression[i] == ')':
+
+        elif char == ')':
             if scopes:
                 scopes.pop()
             i += 1
-        elif expression[i].isupper():
+
+        elif char.isupper():
             i += 1
             if i < n and expression[i] == '(':
                 i += 1
                 while i < n and expression[i] != ')':
                     if expression[i].islower():
                         var = expression[i]
-                        found = False
-                        for scope in reversed(scopes):
-                            if var in scope:
-                                bound_vars.add(var)
-                                found = True
-                                break
-                        if not found:
+                        found = any(var in scope for scope in reversed(scopes))
+                        if found:
+                            bound_vars.add(var)
+                        else:
                             free_vars.add(var)
                     i += 1
                 if i < n and expression[i] == ')':
                     i += 1
+
         else:
             i += 1
 
-    free_vars = free_vars - bound_vars
+    free_vars -= bound_vars
 
-    print(''.join(sorted(free_vars)) if free_vars else 0)
-    print(''.join(sorted(bound_vars)) if bound_vars else 0)
+    print('متغیرهای آزاد:', ''.join(sorted(free_vars)) if free_vars else 0)
+    print('متغیرهای بسته:', ''.join(sorted(bound_vars)) if bound_vars else 0)
 
-examples = [
-    "∀x(P(x)→∃yQ(y,x))",
-    "P(x)∧∃y(Q(y)∨R(x,y))",
-    "∀x(∃y(P(x,y)∧Q(z)))",
-    "P(x,y)∨Q(z)",
-    "∀x(P(x)∧Q(y))",
-]
 
-for idx, expr in enumerate(examples, 1):
-    print(f"Example {idx}: {expr}")
-    extract_variables(expr)
-    print("---")
+# فقط یکبار گرفتن ورودی
+expression = input("لطفاً یک عبارت منطقی وارد کنید: ")
+extract_variables(expression)
